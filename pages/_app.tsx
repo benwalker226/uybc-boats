36383050b30d63f5022a6b9f0f285e0c4b5bd456
benjwalker226 <benjwalker226@gmail.com>
2025-08-06 17:08:57 +0000

    feat: add layout component with header and logo, refactor app to use new layout
    Co-authored-by: Genie <genie@cosine.sh>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,17 @@
 import type { AppProps } from 'next/app';
-import '../styles/globals.css';
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import Layout from '../components/Layout'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
+}
+
+export default MyApp;
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
